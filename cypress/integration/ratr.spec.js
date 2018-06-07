b7e462b0991bb3da64f2b298fd326950eebfedba
Chance Burke <burke@Chances-MacBook-Pro.local>
2018-06-07 13:48:38 -0600

    added Cypress testing with first test to project

---
+++ cypress/integration/ratr.spec.js
@@ -0,0 +1,12 @@
+describe('Fulfill Testing of Bucket 2 Project', function() {
+  it('.should pass all tests', function () {
+    cy.visit('http://localhost:8080/#/')
+    // cy.get('#splash > div > h1').should('have.text', 'Burke Weather!')
+    // cy.get('h3').should('have.length', 2)
+    // cy.get('button').click()
+    // cy.get('h1').should('have.text', 'Burke Weather!')
+    // cy.get('input').click().type('80020').type('{enter}')
+    // cy.get('.city-name').contains('Broomfield')
+    // cy.get('.weather-pic').should('not.have.attr', 'src', '')
+  })
+})
